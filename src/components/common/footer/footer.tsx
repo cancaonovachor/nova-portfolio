2134edbfa0c60655ee461f629b9e0f1541994d11
yudaihomma <kasettoc@gmail.com>
2021-06-13 02:16:23 +0900

    add github actions

---
+++ src/components/common/footer/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {
+  className?: string;
+};
+
+const Component = (props: Props) => {
+  return <></>;
+};
+
+export const Footer: React.FC = () => {
+  return <Component />;
+};
